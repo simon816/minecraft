a2720d48872c67eb2811bf8e13ff585b130417b6
Simon Barnes <simonbarnes1@googlemail.com>
2012-05-31 18:49:13 +0100

    Added Updater

---
+++ mod-downloader/Latest.bat
@@ -0,0 +1,4 @@
+wget "https://raw.github.com/simon816/minecraft/master/mod-downloader/download.bat" -O "download.bat" --no-check-certificate
+wget "https://raw.github.com/simon816/minecraft/master/mod-downloader/7za.exe" -O "7za.exe" --no-check-certificate
+wget "https://raw.github.com/simon816/minecraft/master/mod-downloader/urls.txt" -O "urls.txt" --no-check-certificate
+wget "https://raw.github.com/simon816/minecraft/master/mod-downloader/Latest.bat" -O "Latest.bat" --no-check-certificate
